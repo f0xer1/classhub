fb832238c6b0804272ed74519e7301a37cf1b3a9
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-13 21:43:57 +0200

    Initial commit

---
+++ src/main/java/com/classhub/classhub/ClasshubApplication.java
@@ -0,0 +1,13 @@
+package com.classhub.classhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasshubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classhub/classhub/ClasshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classhub.classhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
