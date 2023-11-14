41f23943901e1019db39b7fd397581d8968c7d07
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-14 18:01:02 +0200

    Add repository

--- src/main/java/com/classhub/api/repository/AdministratorsRepository.java
+++ src/main/java/com/classhub/api/repository/AdministratorsRepository.java
@@ -1,7 +1,10 @@
 package com.classhub.api.repository;
 
-import com.classhub.api.model.Administrators;
+import com.classhub.api.model.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdministratorsRepository extends JpaRepository<Administrators,Long> {
+import java.util.Optional;
+
+public interface AdministratorsRepository extends JpaRepository<Administrator,Long> {
+   Optional<Object> findByUsername(String username);
 }
--- src/main/java/com/classhub/api/repository/TeachersRepository.java
+++ src/main/java/com/classhub/api/repository/TeachersRepository.java
@@ -1,8 +1,11 @@
 package com.classhub.api.repository;
 
-import com.classhub.api.model.Teachers;
+import com.classhub.api.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
-public interface TeachersRepository extends JpaRepository<Teachers, Long> {
+
+public interface TeachersRepository extends JpaRepository<Teacher, Long> {
+    Optional<Object> findByUsername(String username);
 }
