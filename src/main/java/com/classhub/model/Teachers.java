ca16f1dd835f4d01875b749ad845e97481e7409b
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-14 15:56:26 +0200

    Add administrator, student, teacher

---
+++ src/main/java/com/classhub/model/Teachers.java
@@ -0,0 +1,35 @@
+package com.classhub.model;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Teachers")
+public class Teachers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "pwd")
+    private String pwd;
+
+
+
+}
