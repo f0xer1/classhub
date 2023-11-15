4c61775a7c08995843b2e4a1c6fa71a6bac92ad4
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-15 11:59:12 +0200

    Add methods to repository

--- src/main/java/com/classhub/api/repository/UserRepository.java
+++ src/main/java/com/classhub/api/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.classhub.api.repository;
 
-import com.classhub.api.model.User;
+import com.classhub.api.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long > {
     Optional<Object> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
