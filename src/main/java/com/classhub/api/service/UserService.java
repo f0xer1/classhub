bbcd0844a90d4f72f6fdfd0b757913d048a8e8e0
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-15 17:36:57 +0200

    Expanded user service

--- src/main/java/com/classhub/api/service/UserService.java
+++ src/main/java/com/classhub/api/service/UserService.java
@@ -5,8 +5,11 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.classhub.api.model.user.User;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public interface UserService {
-    User signUpForAdmin(User user);
     Optional<DecodedJWT> signIn(String username, String password);
+
+    User signUpForUser(User user);
+
 }
--- src/main/java/com/classhub/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/classhub/api/service/impl/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.classhub.api.service.impl;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.classhub.api.config.JWTTokenProvider;
 import com.classhub.api.exeption.InvalidPasswordException;
+import com.classhub.api.exeption.RoleNotFoundException;
 import com.classhub.api.exeption.UserAlreadyExistsException;
 import com.classhub.api.exeption.UserNotFoundException;
 import com.classhub.api.model.user.User;
 import com.classhub.api.repository.UserRepository;
 import com.classhub.api.service.AdministratorService;
+import com.classhub.api.service.StudentService;
+import com.classhub.api.service.TeacherService;
 import com.classhub.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,18 +25,47 @@ public class UserServiceImpl implements UserService {
     private final JWTTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final AdministratorService administratorService;
+    private final StudentService studentService;
+    private final TeacherService teacherService;
+
+
+    public User signUpForAdmin(User user) {
+        user.setRole("ROLE_ADMINISTRATOR");
+        userRepository.save(user);
+        administratorService.createAdmin(user.getId());
+        return user;
+    }
+
+    public User signUpForStudent(User user) {
+        user.setRole("ROLE_STUDENT");
+        userRepository.save(user);
+        studentService.createStudent(user.getId());
+        return user;
+    }
+
+    public User signUpForTeacher(User user) {
+        user.setRole("ROLE_TEACHER");
+        userRepository.save(user);
+        teacherService.createTeacher(user.getId());
+        return user;
+    }
+
+
 
     @Override
-    public User signUpForAdmin(User user) {
+    public User signUpForUser(User user) {
         if (userRepository.existsByUsername(user.getUsername())) {
             throw new UserAlreadyExistsException(
                     "Username %s is already in use".formatted(user.getUsername()));
         }
-        user.setRole("ROLE_ADMINISTRATOR");
         user.setPwd(passwordEncoder.encode(user.getPwd()));
-        userRepository.save(user);
-        administratorService.createAdmin(user.getId());
-        return user;
+        String role = user.getRole();
+        return switch (role != null ? role.toLowerCase() : "admin") {
+            case "student" -> signUpForStudent(user);
+            case "teacher" -> signUpForTeacher(user);
+            case "admin"  -> signUpForAdmin(user);
+            default -> throw new RoleNotFoundException("Invalid %s role".formatted(role));
+        };
     }
 
     @Override
