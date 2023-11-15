dbc3e86400782b0525d609323b62a701d1888092
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-15 12:24:05 +0200

    Fix methods

--- src/main/java/com/classhub/api/repository/UserRepository.java
+++ src/main/java/com/classhub/api/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long > {
-    Optional<Object> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
 }
