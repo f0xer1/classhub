689b36e8d8a98aaefc0873d9038e090bd1714e78
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-30 13:56:07 +0200

    Add swagger

--- src/main/java/com/classhub/api/controller/AuthController.java
+++ src/main/java/com/classhub/api/controller/AuthController.java
@@ -1,16 +1,23 @@
 package com.classhub.api.controller;
 
 import com.classhub.api.controller.auth.JWTToken;
+import com.classhub.api.model.dto.ExceptionResponse;
 import com.classhub.api.model.mapper.JWTTokenMapper;
 import com.classhub.api.model.dto.User.UserCreationDto;
 import com.classhub.api.model.dto.User.UserDto;
 import com.classhub.api.model.mapper.UserMapper;
 import com.classhub.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path = "/auth")
@@ -19,7 +26,17 @@ public class AuthController {
     private final UserService userService;
     private final UserMapper userMapper;
     private final JWTTokenMapper jwtTokenMapper;
-
+    @Operation(summary = "Sign up new user", responses = {
+            @ApiResponse(responseCode = "201",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = UserDto.class))),
+            @ApiResponse(responseCode = "400",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ExceptionResponse.class))),
+            @ApiResponse(responseCode = "404",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @PostMapping("/sign-up")
     public ResponseEntity<UserDto> signUpForAdmin(@RequestBody @Valid UserCreationDto userDto) {
         var newUser = userService.signUpForAdmin(userMapper.toUser(userDto));
